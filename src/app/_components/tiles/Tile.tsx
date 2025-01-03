2af2bbbbdac4a5e2dc2f6c803a5f62217e93fa7a
Pierre Chabardes <pierre@chabardes.net>
2025-01-03 15:26:46 +0100

    feat(Tiles): add canvas to highlight hexes on Tile

--- src/app/_components/tiles/Tile.tsx
+++ src/app/_components/tiles/Tile.tsx
@@ -1,11 +1,58 @@
-import type { Tile } from '@/domain/tiles.model';
+'use client';
+
+import type { Hex, Tile } from '@/domain/tiles.model';
 import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+
+const RADIUS = 58;
+
+function drawCircleArea(hex: Hex, context: CanvasRenderingContext2D) {
+  const { x, y } = hex;
+
+  context.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+  context.lineWidth = 3;
+  context.beginPath();
+  context.arc(x, y, RADIUS, 0, 2 * Math.PI);
+  context.stroke();
+}
 
 export default function TileComponent({ tile }: { tile: Tile }) {
-  return <Image
-    src={tile.path}
-    alt={tile.id}
-    width={tile.size.width}
-    height={tile.size.height}
-  />;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [currentHex, setCurrentHex] = useState<Hex>();
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+    context.reset();
+    if (currentHex === undefined) return;
+    drawCircleArea(currentHex, context);
+  }, [currentHex]);
+
+  return <div className='relative'>
+    <canvas
+      ref={canvasRef}
+      className='absolute pointer-events-none'
+      width={tile.size.width}
+      height={tile.size.height}
+    />
+    <map name={tile.id}>
+      {tile.hexes.map((hex, index) => {
+        return <area
+          key={`${tile.id}-${index}`}
+          coords={`${hex.x},${hex.y},${RADIUS}`}
+          shape='circle'
+          onMouseEnter={() => setCurrentHex(hex)}
+        />;
+      })}
+    </map>
+    <Image
+      useMap={`#${tile.id}`}
+      src={tile.path}
+      alt={tile.id}
+      width={tile.size.width}
+      height={tile.size.height}
+    />
+  </div>;
 }
--- src/domain/tiles.model.ts
+++ src/domain/tiles.model.ts
@@ -10,5 +10,5 @@ export interface Tile {
     width: number;
     height: number;
   };
-  hex: Hex[];
+  hexes: Hex[];
 }
